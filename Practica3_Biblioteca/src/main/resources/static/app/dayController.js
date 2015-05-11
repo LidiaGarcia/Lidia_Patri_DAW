9ad6ab120872a371c02aeaab2724f69fba5acf56
alumno <pb.cahue@alumno.urjc.es>
2015-05-11 20:42:14 +0200

    diaC·

---
+++ Practica3_Biblioteca/src/main/resources/static/app/dayController.js
@@ -0,0 +1,49 @@
+angular.module("app").controller("DayController", DayController);
+
+DayController.$inject = ["globalService","$location"];
+
+function DayController(globalService, $location) {
+
+	var vm = this;
+	vm.cursos= globalService.getCursosDia();
+	vm.salas= globalService.getSalas();
+	vm.portatiles= globalService.getPortatiles();
+	vm.reservasSala = globalService.getReservasSalaDia();
+	vm.reservasPortatil = globalService.getReservasPortatilDia();
+	vm.islog=globalService.islog();
+	vm.carga=false;
+	vm.disponiblesSala=0;
+	vm.disponiblesPortatil=0;
+	vm.numhoras=12;
+	
+	vm.reload = function (){
+		globalService.reload();
+		setTimeout(function(){
+			vm.cursos= globalService.getCursosDia();		
+			vm.salas= globalService.getSalas();
+			vm.portatiles= globalService.getPortatiles();
+			vm.reservasSala = globalService.reservasSalaHoy;
+			vm.reservasPortatil = globalService.reservasPortatilHoy;
+			
+			}
+		,1900);
+		setTimeout(function(){
+			vm.reservasSalaDia();
+			vm.reservasPortatilDia();
+			vm.carga=true;
+			}
+		,2200);
+
+	}
+	vm.reservasSalaDia = function(){
+	
+		vm.disponiblesSala = vm.salas.length*vm.numhoras - vm.reservasSala.length;
+	}
+	vm.reservasPortatilDia = function(){
+		vm.disponiblesPortatil = vm.portatiles.length*vm.numhoras - vm.reservasPortatil.length;
+	}
+	
+	setTimeout(function(){
+		vm.reload();}
+	,200);
+};
